0f5e214f8f4c71d3c6df31fb2fc37cd425ec3b8f
Cynthia Mojica <cmojica1231@gmail.com>
2020-09-11 16:30:19 -0700

    modified css and index

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,7 @@
 var citySearch;
 var APIkey = "&appid=28870b55a52a06273a2463ffab2469f7";
 var weatherAPI = "https://api.openweathermap.org/data/2.5/weather?";
-var uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
+uviAPI = "https://api.openweathermap.org/data/2.5/uvi?lat=";
 var forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?q=";
 var geoAPI = navigator.geolocation;
 var units = "&units=imperial";
